464c9ee92874b56919fcf9e3717408c0109562c0
FArooghOghba <faroogh.oghba@gmail.com>
2022-07-31 00:21:01 +0430

    feat: add blog list

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from blog.models import *
+
+
 # Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'published_date'
+    ]
+
+    prepopulated_fields = {
+        'slug': ['title']
+    }
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+from .models import Blog
 
 
 # Create your views here.
 
 def blog_list_view(request):
-    return render(request, template_name='blog/blog_list.html')
+    blogs = Blog.objects.all()
+
+    paginator = Paginator(blogs, 3)
+    page_number = request.GET.get('page')
+    try:
+        all_post_pages = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        all_post_pages = paginator.get_page(1)
+    except EmptyPage:
+        all_post_pages = paginator.get_page(paginator.num_pages)
+
+    context = {'all_post_pages': all_post_pages}
+    return render(request, template_name='blog/blog_list.html', context=context)
 
 
 def blog_single_view(request):
