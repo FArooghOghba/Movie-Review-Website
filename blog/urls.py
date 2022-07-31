910376a9ea71ef344dc8960aeb7848f6b72b01f5
FArooghOghba <faroogh.oghba@gmail.com>
2022-07-31 18:45:59 +0430

    feat: blog search.
    adding blog search view with template tag.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_list_view, name='list'),
     path('single/<slug:post_slug>', blog_single_view, name='single'),
+
+    path('search/', blog_search_view, name='search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 
 from .models import Post
 
@@ -27,3 +28,26 @@ def blog_single_view(request, post_slug):
 
     context = {'post': post}
     return render(request, template_name='blog/blog_single.html', context=context)
+
+
+def blog_search_view(request):
+    if request.method == 'GET':
+        if search := request.GET.get('search'):
+            posts = Post.objects.filter(
+                Q(title__icontains=search) |
+                Q(content__icontains=search)
+            )
+
+            paginator = Paginator(posts, 3)
+            page_number = request.GET.get('page')
+            try:
+                all_post_pages = paginator.get_page(page_number)
+            except PageNotAnInteger:
+                all_post_pages = paginator.get_page(1)
+            except EmptyPage:
+                all_post_pages = paginator.get_page(paginator.num_pages)
+
+            context = {'all_post_pages': all_post_pages}
+            return render(request, template_name='blog/blog_list.html', context=context)
+
+    return redirect('blog:list')
