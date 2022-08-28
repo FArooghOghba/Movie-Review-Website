808cde51894623e33c75ebad1aee0f2e9b3ff7dc
FArooghOghba <faroogh.oghba@gmail.com>
2022-08-29 01:16:25 +0430

    fix: show form errors in modal.

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
 
 class CustomAuthenticationForm(AuthenticationForm):
+
+    def clean_username(self):
+        username = self.data['username']
+        if '@' in username:
+            try:
+                username = User.objects.get(email=username).username
+            except ObjectDoesNotExist:
+                raise ValidationError(
+                    self.error_messages['invalid_login'],
+                    code='invalid_login',
+                    params={'username': self.username_field.verbose_name},
+                )
+        return username
+
     class Meta:
         model = User
         fields = ['username', 'password']
